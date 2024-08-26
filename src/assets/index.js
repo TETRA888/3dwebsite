c4b0a29dc63a962cc811438056b1da06f7e12ff1
Asad <tetragamedevelopment@gmail.com>
2024-08-25 20:44:20 -0700

    add experience section

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import meta from "./company/blender.png";
+import shopify from "./company/ucdavis.png";
+import starbucks from "./company/ucprc.png";
+import tesla from "./company/gsm.PNG";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
